9ea9221f99fa6e24b6c7f7bc7388f9308b67d2c2
OwenTsang <i.hwzeng@gmail.com>
2013-08-04 17:38:45 +0800

    Initial Commit

---
+++ TestEncryption/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  TestEncryptionAppController.h
+//  TestEncryption
+//
+//  Created by OwenTsang on 8/4/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ TestEncryption/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestEncryptionAppController.h
+//  TestEncryption
+//
+//  Created by OwenTsang on 8/4/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
